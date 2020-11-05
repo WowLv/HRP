4a62ca232022ce3f6e9cd74d6979d0a505e261aa
WowLv <lyuhongzhi@foxmail.com>
2020-11-05 22:54:04 +0800

    备案

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
     <router-view />
+    <div style="width:300px;margin:0 auto; padding:20px 0;">
+      <a
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44060502002277"
+        style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"
+        ><img src="./assets/beian.png" style="float:left;" />
+        <p
+          style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;"
+        >
+          粤公网安备 44060502002277号
+        </p></a
+      >
+    </div>
   </div>
 </template>
 
